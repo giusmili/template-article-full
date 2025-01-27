474b629bb1c849908bbbb7dee3da02fe045bf412
giusmili <giusmili67@gmail.com>
2025-01-27 17:47:30 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
+import {packages} from './modele.js'
+
 document.addEventListener("DOMContentLoaded", e =>{
     e.preventDefault()
     
-    const el = document.querySelector("html")
-    console.info(el.children[0].childNodes[1].getAttribute("charset"))
-    console.log(el.setAttribute("lang","en"))
+    packages()
     
 })
 
---
+++ js/modele.js
@@ -0,0 +1,10 @@
+ export const packages = () =>{
+    const el = document.querySelector("html")
+    console.info(el.children[0].childNodes[1].getAttribute("charset"))
+    console.log(el.setAttribute("lang","en"))
+    Swal.fire({
+        title: `Votre langue : ${navigator.language}`,
+        text: "You clicked the button!",
+        icon: "success"
+      });
+ }
