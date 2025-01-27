1bc381087974a1ced3f221364c767474de48389e
giusmili <giusmili67@gmail.com>
2025-01-27 17:56:57 +0100

    promise

--- js/modele.js
+++ js/modele.js
@@ -2,9 +2,9 @@
     const el = document.querySelector("html")
     console.info(el.children[0].childNodes[1].getAttribute("charset"))
     console.log(el.setAttribute("lang","en"))
-    Swal.fire({
+   /*  Swal.fire({
         title: `Votre langue : ${navigator.language}`,
         text: "You clicked the button!",
         icon: "success"
-      });
+      }); */
  }
